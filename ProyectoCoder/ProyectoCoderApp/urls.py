47cd7962cdc6225edd8bdb5463d7b54b6addd525
MartinCapocasale <martincapocasale@gmail.com>
2022-06-20 19:43:57 -0300

    se modifican las urls url.py de la app

--- ProyectoCoder/ProyectoCoderApp/urls.py
+++ ProyectoCoder/ProyectoCoderApp/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',inicio),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('cursos/', cursos),
-    path('entregables/', entregables),
-    path('base/', base),
+    path('',inicio,name = "inicio"),
+    path('profesores/', profesores, name = "profesores"),
+    path('estudiantes/', estudiantes, name = "estudiantes"),
+    path('cursos/', cursos, name = "cursos"),
+    path('entregables/', entregables, name = "entregables"),
+    #/path('base/', base),
 ]
